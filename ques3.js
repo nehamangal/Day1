988c1cdfe8f743e4b2fd21bcba06cc37d5910505
nehamangal <nehamangal702@gmail.com>
2022-08-09 17:21:03 +0530

    first commit

---
+++ ques3.js
@@ -0,0 +1,19 @@
+var _ = require('lodash')
+const members = [
+    {name: 'Laveesh Gupta', age: 20},
+    {name: 'Yash Jangid', age: 40},
+    {name: 'Firoz Khan', age: 41},
+    {name: 'Amrit Srivastava', age: 17},
+    {name: 'Chandraprakash Sharma'},
+    {name: 'Swpril Ahuja', age: 45},
+    {name: 'Yogesh Khatri', age: 51}
+  ]
+
+ var arr = []
+  var res = _.map(members,function get(value){
+       if(value.age>=41 && value.age<=60){
+        arr.push(value)
+       }
+  })
+
+  console.log(arr)
---
+++ ques8.js
@@ -0,0 +1,19 @@
+var _ = require('lodash')
+const members = [
+    {name: 'Laveesh Gupta', age: 20},
+    {name: 'Yash Jangid', age: 40},
+    {name: 'Firoz Khan', age: 41},
+    {name: 'Amrit Srivastava', age: 17},
+    {name: 'Chandraprakash Sharma'},
+    {name: 'Swpril Ahuja', age: 45},
+    {name: 'Yogesh Khatri', age: 51}
+  ]
+
+var count=0;
+var res = _.map(members,function enets(value){
+  count++;
+  if(count<=2){
+    console.log(value)
+  }
+  
+})
